195c599555b998d6edb3cedd8f69016166f1ab3e
msbaek <msbaek@daumcorp.com>
2013-08-29 16:06:31 +0900

    move

--- src/test/java/net/codetemplate/uda/domain/LoginInteractorTest.java
+++ src/test/java/net/codetemplate/uda/domain/LoginInteractorTest.java
@@ -39,20 +39,4 @@ public class LoginInteractorTest {
         verifySuccessCondition();
     }
 
-    private class LoginInteractor {
-        private final UserRepository userRepository;
-        private final LoginPresenter loginPresenter;
-
-        public LoginInteractor(UserRepository userRepository, LoginPresenter loginPresenter) {
-            this.userRepository = userRepository;
-            this.loginPresenter = loginPresenter;
-        }
-
-        public void execute(LoginRequest loginRequest) {
-            User user = userRepository.findBy(loginRequest.getId());
-            user.login(loginRequest.getPassword());
-            LoginResponse loginResponse = new LoginResponse(user.getId());
-            loginPresenter.present(loginResponse);
-        }
-    }
 }
